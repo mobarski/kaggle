e0742ebfa0f86f26962142f88b2ed14924cf9237
mobarski <mobarski@gmail.com>
2018-03-29 21:29:36 +0200

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kaggle',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/mobarski/kaggle',
+      author='Maciej Obarski',
+      author_email='mobarski@gmail.com',
+      license='MIT',
+      packages=['kaggle'],
+      zip_safe=False)
